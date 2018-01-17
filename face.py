d9b1e2ac67674ba0412c561fcce1d4834ab9aafa
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-17 08:25:16 -0500

    Updated from Brython Server: 1/17/2018 8:25:16 AM

--- face.py
+++ face.py
@@ -1,4 +1,11 @@
-print('           xxxxx')
-print('       x            x')
-print('      ((    o  o    ))')
-print('          |  V
+#Christo
+#1/17/18
+#face.py
+
+
+print('          xxxxx')
+print('       x          x')
+print('      ((   o  o   ))')
+print('           |V|')
+print('          |===|')
+print('          -----')
