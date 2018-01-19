67eeb36add326eefe9fed08bce9c33f61a6434b1
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-19 08:40:09 -0500

    Updated from Brython Server: 1/19/2018 8:40:08 AM

--- slope.py
+++ slope.py
@@ -1,1 +1,22 @@
+#Christo Dragnev
+#1/19/18
+#slope.py
+
+x1 = float(input('Enter x1: ')) 
+y1 = float(input('Enter y1: '))
+x2 = float(input('Enter x2: '))
+y2 = float(input('Enter y2: '))
+
+print('equation is',(y1-y2)/(x1-x2),'x +',(y1-(((y1-y2)/(x1-x2))*x1)))
+
+
+
+
+
+
+
+
+
+
+  
 
