2072fafe46f53c16e59e3fccdbc1deae03bcd19a
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-22 08:55:13 -0500

    Updated from Brython Server: 1/22/2018 8:55:15 AM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,9 @@
+#Christo Dragnev
+#1/22/18
+#eggs.py
 
+numberOfEggs = int(input('Enter number of eggs: '))
+numberOfEggCartons = int(input('Enter number of egg cartons: '))
+
+eggCartons<=12
+print('True', 
