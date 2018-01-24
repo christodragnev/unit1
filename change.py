c9e7692a4482a8b84e86c96e0765b27a9f1a6f52
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-24 08:40:19 -0500

    Updated from Brython Server: 1/24/2018 8:40:21 AM

--- change.py
+++ change.py
@@ -1,1 +1,12 @@
+numberOfCents = int(input('Enter number of cents:'))
 
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1
+
+numberOfCents-((numberOfCents//quarter)*quarter)
+
+numberOfQuarters = print('Quarters:',(numberOfCents//quarter))
+numberOfDimes = print('Dimes:',((numberOfCents-((numberOfCents//quarter)*quarter))//dime))
+numberOfNickels = print('Nickels', (numberOfCents-((numberOfCents//quarter)*quarter))-((numberOfCents//dime)*dime)//nickel)
