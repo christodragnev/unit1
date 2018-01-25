b36a5c64144eec58b9282fbaf90c62e1555f4e5a
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-25 08:33:01 -0500

    Updated from Brython Server: 1/25/2018 8:33:00 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,9 @@
+A = float(input('Enter Side A: '))
+B = float(input('Enter Side B: '))
+C = float(input('Enter Side C: '))
 
+long = max(A,B,C)
+short = min(A,B,C)
+middle = (A*B*C)/(long*short)
+
+print(short+middle>long)
