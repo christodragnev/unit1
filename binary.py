dcd47712fff9e8d16cb304d3cccfbda0b6473cc5
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-25 09:41:02 -0500

    Updated from Brython Server: 1/25/2018 9:41:02 AM

--- binary.py
+++ binary.py
@@ -6,17 +6,15 @@ binaryNumber = int(input('Enter an 8-digit binary number: '))
 
 A = binaryNumber//10000000
 B = (binaryNumber/10)//1000000
-C = (binaryNumber/10)//100000
+C = (binaryNumber/100)//100000
 D = (binaryNumber/1000)//10000
-E = (binaryNumber/10000)//10000
-F = (binaryNumber/100000)//1000
-G = (binaryNumber/1000000)//100
-H = (binaryNumber/10000000)//10
+E = (binaryNumber/10000)//1000
+F = (binaryNumber/100000)//100
+G = (binaryNumber/1000000)//10
+H = (binaryNumber/10000000)//1
 
-base10= (A*(2^7))+(B*(2^6))+(C*(2^5))+(D*(2^4))+(E*(2^3))+(F*(2^2))+(G*(2))+(H)
+base10 = (A*(2^7))+(B*(2^6))+(C*(2^5))+(D*(2^4))+(E*(2^3))+(F*(2^2))+(G*(2))+(H)
 
 print(base10)
 
-
-print(B)
-print(C)
+print((binaryNumber/10)//1000000)
