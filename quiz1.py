1df89b7695187b08593d479753078ba11f321171
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-26 08:16:18 -0500

    Updated from Brython Server: 1/26/2018 8:16:17 AM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,11 @@
+#Christo Dragnev
+#1/26/18
+#quiz1.py
 
+print('Hello')
+
+name = input('Enter your name: ')
+favNumber1 = float(input('Enter your favorite number: '))
+print(name*5)
+favNumber2 =  float(input('Enter your second favorite number: '))
+luckyNumber = print('Your favorite number is', favNumber1**favNumber2)
